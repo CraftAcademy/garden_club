a1956115d083286f72e22275c0d42282cd1e9e01
Amber <amber@amberwilkie.com>
2016-09-13 21:16:44 +0200

    Users may update profile with garden website HECK YEAH

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
   root controller: :home, action: :index
   get 'profile', controller: :profile, action: :index
+
   # get 'users', controller: :profile, action: :index
 end
