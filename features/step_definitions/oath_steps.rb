eb12cc03d322f3aa358e917c467c4c8f5fee9d50
Amber <amber@amberwilkie.com>
2016-09-14 19:50:16 +0200

    Add some clarifying comments

--- features/step_definitions/oath_steps.rb
+++ features/step_definitions/oath_steps.rb
@@ -1,15 +1,19 @@
-OmniAuth.config.test_mode = true
+# this needs to be false if you're trying to test actual FB login functionality on the local server. Otherwise, keep it true to prevent us throwing a thousand requests at FB
+OmniAuth.config.test_mode = false
 OmniAuth.config.mock_auth[:facebook] = nil
-OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new({
-  provider: 'facebook',
-  uid: '123545',
-  info: {
-    email: 'joe@bloggs.com',
-    name: 'Joe Bloggs',
-    image: 'http://graph.facebook.com/1234567/picture?type=square',
-    verified: true
-        }
-  })
+OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new({"provider"=>"facebook",
+ "uid"=>"10101143923045869",
+ "info"=>
+  {"email"=>"joe@boggs.com",
+   "name"=>"Joe Boggs",
+   "image"=>"http://graph.facebook.com/v2.6/10101143923045869/picture"},
+ "credentials"=>
+  {"token"=>
+    "EAATRfvIVFs0BAP3uTezasAFPbu8zLbmOyFo28Te663JGzSciMM39idcvdXzWMbahlNJYABSwhQLLvZCBZBoZC6dZB91EdnZC8oPT8I58uHMxlnYQm2RugGgxkn2VJRPLX8WWwZBoFZA53ZASx3qaiiLKRszVpm8RmJUZD",
+   "expires_at"=>1479048165,
+   "expires"=>true},
+ "extra"=>{"raw_info"=>{"name"=>"Amber Wilkie", "email"=>"theamb@gmail.com", "id"=>"10101143923045869"}}
+})
 
 When(/^I click the Sign in with Facebook link$/) do
   click_link('Sign in with Facebook')
