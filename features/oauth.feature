35d8b74cc1a9b02df5f164fb3b32c4b379fc97c7
Amber <amber@amberwilkie.com>
2016-09-14 14:36:59 +0200

    Write cuke tests for getting to Facebook login page

---
+++ features/oauth.feature
@@ -0,0 +1,9 @@
+Feature: As a user
+  in order to facilitate login and registration
+  I would like to sign up and login with Facebook.
+
+Scenario:
+  Given I am on the "Login" page
+  Then I should see "Sign in with Facebook"
+  When I click the "Sign in with Facebook" link
+  Then I should be on the "Facebook Login" page
--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,5 +1,7 @@
 Given(/^I am on the "([^"]*)" page$/) do |page|
-  visit new_user_registration_path
+  set_goto(page)
+  visit @goto
+  expect(current_path).to eq @goto
 end
 
 Then(/^I should see "([^"]*)"$/) do |text|
@@ -14,6 +16,20 @@ When(/^I click the "([^"]*)" button$/) do |button|
   click_button(button)
 end
 
+When(/^I click the "([^"]*)" link$/) do |link|
+  click_link(link)
+end
+
 Then(/^I should be on the "([^"]*)" page$/) do |page|
   expect(current_path).to eq registration_index_path
+end
+
+def set_goto(page)
+  @goto = user_facebook_omniauth_authorize if page == "Facebook Login"
+  @goto = root_path if page == "home"
+  @goto = new_user_session_path if page == "Login"
+  @goto = new_user_registration_path if page == "registration"
+  @goto = edit_user_registration_path if page == "Edit Profile"
+  @goto = profile_path if page == "Profile"
+  @goto = private_path if page == "private information"
 end
