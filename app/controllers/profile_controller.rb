0238b96679a91af9665e1da793a1ee5c5185b6f8
Amber <amber@amberwilkie.com>
2016-09-17 17:58:46 +0200

    Append http:// to garden websites if they lack it, so they actually link

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,4 +1,11 @@
 class ProfileController < ApplicationController
   def index
+    @user = User.find_by_id(params[:user_id])
+    @user_garden_website = url_with_protocol(@user.garden_website)
+  end
+
+  private
+  def url_with_protocol(url)
+    /^http/i.match(url) ? url : "http://#{url}"
   end
 end
