1579ae3e02f99eb3e49a9ed99f1d5f8c287d4ad2
Amber <amber@amberwilkie.com>
2016-09-17 06:33:14 +0200

    Allow for tag_list to hit the Article model

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -30,7 +30,7 @@ class ArticlesController < ApplicationController
 
   private
   def article_params
-     params.require(:article).permit(:title, :body, :user_id)
+     params.require(:article).permit(:title, :body, :user_id, :tag_list)
   end
 
   def error_message
