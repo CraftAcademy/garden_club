573dbf87e6a4b42df82143fecf69eca2743526e9
Amber <amber@amberwilkie.com>
2016-09-17 20:03:01 +0200

    Add JS and div to profile page

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,4 +1,8 @@
 class ProfileController < ApplicationController
   def index
+    @user = User.find_by_id(params[:user_id])
+    if @user.latitude.nil? && @user.g_city != nil
+      @user.geocode
+    end
   end
 end
