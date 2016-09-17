8e2d0687049c50900063ca751be94f0b5eacd0e9
Amber <amber@amberwilkie.com>
2016-09-17 20:45:25 +0200

    Display garden map on user page

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -4,5 +4,9 @@ class ProfileController < ApplicationController
     if @user.latitude.nil? && @user.g_city != nil
       @user.geocode
     end
+    @map_hash = Gmaps4rails.build_markers(@user) do |user, marker|
+      marker.lat user.latitude
+      marker.lng user.longitude
+    end
   end
 end
